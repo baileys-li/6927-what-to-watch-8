07fa55cfb44950b9685ffeeaa79acab482b758cd
Arthur Baileys Li <arthurlitovko@gmail.com>
2021-10-01 02:54:59 +0300

    Add sass styles for small card

--- project/src/components/small-film-card/small-film-card.tsx
+++ project/src/components/small-film-card/small-film-card.tsx
@@ -1,16 +1,21 @@
+import s from './small-film-card.module.scss';
+
 type SmallFilmCardProps = {
   title: string;
   imageSource: string;
 };
 
-function SmallFilmCard({ title, imageSource }: SmallFilmCardProps): JSX.Element {
+function SmallFilmCard({
+  title,
+  imageSource,
+}: SmallFilmCardProps): JSX.Element {
   return (
-    <article className="small-film-card catalog__films-card">
-      <div className="small-film-card__image">
-        <img src={imageSource} alt="Poster of movie" width="280" height="175" />
+    <article className={`${s.card} catalog__films-card`}>
+      <div className={s.card__image}>
+        <img src={imageSource} alt='Poster of movie' width='280' height='175' />
       </div>
-      <h3 className="small-film-card__title">
-        <a className="small-film-card__link" href="film-page.html">
+      <h3 className={s.card__title}>
+        <a className={s.card__link} href='film-page.html'>
           {title}
         </a>
       </h3>
