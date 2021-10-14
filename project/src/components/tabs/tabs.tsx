bb9a230335ed44abf79bbc1d9d1a8f6acf232d7f
Arthur Baileys Li <arthurlitovko@gmail.com>
2021-10-14 04:01:43 +0300

    Use Ref for Tabs states

--- project/src/components/tabs/tabs.tsx
+++ project/src/components/tabs/tabs.tsx
@@ -1,6 +1,6 @@
-import { ReactElement, useState, KeyboardEvent } from 'react';
+import { ReactElement, useState, KeyboardEvent, useRef } from 'react';
 
-import './tabs.scss';
+import './tabs.module.scss';
 
 type TabsType = {
   children: Array<ReactElement>;
@@ -12,38 +12,48 @@ function Tabs({ children, navigation }: TabsType): JSX.Element {
     text.toLowerCase().replace(' ', '_'),
   );
 
+  const selectedPanel = useRef<HTMLElement | null>(null);
+
+  const navigationRef = useRef<HTMLUListElement | null>(null);
   const [selected, setSelected] = useState(0);
 
   function navigateByKeys(evt: KeyboardEvent) {
     switch (evt.key) {
       case 'ArrowLeft':
-        selected !== 0 && setSelected(selected - 1);
+        if (selected !== 0) {
+          const target: number = selected - 1;
+          switchTab(target);
+        }
         break;
 
       case 'ArrowRight':
-        selected !== normalizedNav.length - 1 && setSelected(selected + 1);
+        if (selected !== normalizedNav.length - 1) {
+          const target: number = selected + 1;
+          switchTab(target);
+        }
         break;
 
       case 'ArrowDown':
-        focusSelectedPanel();
+        selectedPanel?.current?.focus();
         break;
       default:
         break;
     }
   }
 
-  function focusSelectedPanel() {
-    const panel: null | HTMLElement = document.querySelector(
-      `#${normalizedNav[selected]}_panel`,
-    );
-    panel && panel.focus();
+  function switchTab(target: number) {
+    setSelected(target);
+
+    (
+      navigationRef.current?.children[target].children[0] as HTMLElement
+    ).focus();
   }
 
   return (
     <article className='tabs'>
-      <ul role='tablist'>
+      <ul role='tablist' ref={navigationRef}>
         {normalizedNav.map((tab, index) => (
-          <li key={tab} role='presentation' tabIndex={-1}>
+          <li key={tab} role='presentation'>
             <button
               type='button'
               id={tab}
@@ -51,6 +61,7 @@ function Tabs({ children, navigation }: TabsType): JSX.Element {
               aria-selected={index === selected}
               onClick={() => setSelected(index)}
               onKeyDown={navigateByKeys}
+              tabIndex={index === selected ? 0 : -1}
             >
               {navigation[index]}
             </button>
@@ -60,6 +71,7 @@ function Tabs({ children, navigation }: TabsType): JSX.Element {
       {normalizedNav.map((tab, index) => (
         <section
           key={`${tab}_panel`}
+          ref={index === selected ? selectedPanel : null}
           id={`${tab}_panel`}
           role='tabpanel'
           tabIndex={-1}
@@ -74,77 +86,3 @@ function Tabs({ children, navigation }: TabsType): JSX.Element {
 }
 
 export default Tabs;
-/*
-
-(function() {
-  // Get relevant elements and collections
-  const tabbed = document.querySelector('.tabbed');
-  const tablist = tabbed.querySelector('ul');
-  const tabs = tablist.querySelectorAll('a');
-  const panels = tabbed.querySelectorAll('[id^="section"]');
-
-  // The tab switching function
-  const switchTab = (oldTab, newTab) => {
-    newTab.focus();
-    // Make the active tab focusable by the user (Tab key)
-    newTab.removeAttribute('tabindex');
-    // Set the selected state
-    newTab.setAttribute('aria-selected', 'true');
-    oldTab.removeAttribute('aria-selected');
-    oldTab.setAttribute('tabindex', '-1');
-    // Get the indices of the new and old tabs to find the correct
-    // tab panels to show and hide
-    let index = Array.prototype.indexOf.call(tabs, newTab);
-    let oldIndex = Array.prototype.indexOf.call(tabs, oldTab);
-    panels[oldIndex].hidden = true;
-    panels[index].hidden = false;
-  }
-
-  // Add the tablist role to the first <ul> in the .tabbed container
-  tablist.setAttribute('role', 'tablist');
-
-  // Add semantics are remove user focusability for each tab
-  Array.prototype.forEach.call(tabs, (tab, i) => {
-
-    tab.setAttribute('id', 'tab' + (i + 1));
-    tab.parentNode.setAttribute('role', 'presentation');
-
-    // Handle clicking of tabs for mouse users
-    tab.addEventListener('click', e => {
-      e.preventDefault();
-      let currentTab = tablist.querySelector('[aria-selected]');
-      if (e.currentTarget !== currentTab) {
-        switchTab(currentTab, e.currentTarget);
-      }
-    });
-
-    // Handle keydown events for keyboard users
-    tab.addEventListener('keydown', e => {
-      // Get the index of the current tab in the tabs node list
-      let index = Array.prototype.indexOf.call(tabs, e.currentTarget);
-      // Work out which key the user is pressing and
-      // Calculate the new tab's index where appropriate
-      let dir = e.which === 37 ? index - 1 : e.which === 39 ? index + 1 : e.which === 40 ? 'down' : null;
-      if (dir !== null) {
-        e.preventDefault();
-        // If the down key is pressed, move focus to the open panel,
-        // otherwise switch to the adjacent tab
-        dir === 'down' ? panels[i].focus() : tabs[dir] ? switchTab(e.currentTarget, tabs[dir]) : void 0;
-      }
-    });
-  });
-
-  // Add tab panel semantics and hide them all
-  Array.prototype.forEach.call(panels, (panel, i) => {
-    panel.setAttribute('role', 'tabpanel');
-    panel.setAttribute('tabindex', '-1');
-    let id = panel.getAttribute('id');
-    panel.setAttribute('aria-labelledby', tabs[i].id);
-    panel.hidden = true;
-  });
-
-  // Initially activate the first tab and reveal the first tab panel
-  tabs[0].removeAttribute('tabindex');
-  tabs[0].setAttribute('aria-selected', 'true');
-  panels[0].hidden = false;
-})(); */
