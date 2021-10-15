651f8631e2e7992e3dda174e5ba450d6e4074bc1
Arthur Baileys Li <arthurlitovko@gmail.com>
2021-10-15 20:27:51 +0300

    Rework form styles

--- project/src/components/review-form/review-form.tsx
+++ project/src/components/review-form/review-form.tsx
@@ -1,43 +1,40 @@
+import style from './review-form.module.scss';
+
 const STARS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].reverse();
 function ReviewForm(): JSX.Element {
   return (
-    <div className='add-review'>
-      <form action='#' className='add-review__form'>
-        <div className='rating'>
-          <div className='rating__stars'>
-            {STARS.map((star) => (
-              <>
-                <input
-                  className='rating__input'
-                  id={`star-${star}`}
-                  type='radio'
-                  name='rating'
-                  value={star}
-                  defaultChecked={star === 8}
-                />
-                <label className='rating__label' htmlFor={`star-${star}`}>
-                  Rating {star}
-                </label>
-              </>
-            ))}
-          </div>
-        </div>
+    <form className={style.form} action='#'>
+      <div className={style.stars}>
+        {STARS.map((star) => (
+          <>
+            <input
+              className={style.star}
+              id={`star-${star}`}
+              type='radio'
+              name='rating'
+              value={star}
+              defaultChecked={star === 8}
+            />
+            <label className={style.star__label} htmlFor={`star-${star}`}>
+              Rating {star}
+            </label>
+          </>
+        ))}
+      </div>
 
-        <div className='add-review__text'>
-          <textarea
-            className='add-review__textarea'
-            name='review-text'
-            id='review-text'
-            placeholder='Review text'
-          />
-          <div className='add-review__submit'>
-            <button className='add-review__btn' type='submit'>
-              Post
-            </button>
-          </div>
-        </div>
-      </form>
-    </div>
+      <div className={style.text}>
+        <textarea
+          className={style.textarea}
+          name='review-text'
+          id='review-text'
+          placeholder='Review text'
+        />
+
+        <button className={style.button} type='submit'>
+          Post
+        </button>
+      </div>
+    </form>
   );
 }
 
