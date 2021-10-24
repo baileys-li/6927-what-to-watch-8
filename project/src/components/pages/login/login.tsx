662e5c6efea6b9775848576cabf6abc2e060bc60
Arthur Baileys Li <arthurlitovko@gmail.com>
2021-10-24 21:30:11 +0300

    Update login form style

--- project/src/components/pages/login/login.tsx
+++ project/src/components/pages/login/login.tsx
@@ -1,54 +1,43 @@
 import Header from '../../header/header';
 import Footer from '../../footer/footer';
+import style from './login.module.scss';
 
 function Login(): JSX.Element {
+  const message = 'Please enter a valid email address';
   return (
     <div className='user-page'>
-      <Header
-        className='user-page__head'
-        headline='Sign in'
-        hideAuth
-      />
+      <Header className='user-page__head' headline='Sign in' hideAuth />
 
-      <div className='sign-in user-page__content'>
-        <form action='#' className='sign-in__form'>
-          <div className='sign-in__fields'>
-            <div className='sign-in__field'>
+      <div className={`${style.wrapper} user-page__content`}>
+        <form action='#' className={style.form}>
+          {message && <p className={style.message}>{message}</p>}
+          <fieldset className={style.fields}>
+            <legend className='visually-hidden'>Login Form</legend>
+            <label>
+              <span className='visually-hidden'>Email</span>
               <input
-                className='sign-in__input'
+                className={style.input}
                 type='email'
                 placeholder='Email address'
                 name='user-email'
-                id='user-email'
+                required
               />
-              <label
-                className='sign-in__label visually-hidden'
-                htmlFor='user-email'
-              >
-                Email address
-              </label>
-            </div>
-            <div className='sign-in__field'>
+            </label>
+
+            <label>
+              <span className='visually-hidden'>Password</span>
               <input
-                className='sign-in__input'
+                className={style.input}
                 type='password'
                 placeholder='Password'
                 name='user-password'
-                id='user-password'
+                required
               />
-              <label
-                className='sign-in__label visually-hidden'
-                htmlFor='user-password'
-              >
-                Password
-              </label>
-            </div>
-          </div>
-          <div className='sign-in__submit'>
-            <button className='sign-in__btn' type='submit'>
+            </label>
+          </fieldset>
+          <button className={style.submit} type='submit'>
               Sign in
-            </button>
-          </div>
+          </button>
         </form>
       </div>
 
