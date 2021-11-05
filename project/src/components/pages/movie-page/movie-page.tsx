0013d448e2c4d211e5c9c282fc46c2a24e554742
Arthur Baileys Li <arthurlitovko@gmail.com>
2021-11-05 02:25:25 +0200

    Hide errors [temp]

--- project/src/components/pages/movie-page/movie-page.tsx
+++ project/src/components/pages/movie-page/movie-page.tsx
@@ -2,11 +2,11 @@ import Footer from '../../footer/footer';
 import PromoFilm from '../../promo-film/promo-film';
 import Catalog from '../../catalog/catalog';
 
-import { useParams } from 'react-router';
-import type RouteParams from '../../../types/route-params-type';
+// import { useParams } from 'react-router';
+// import type RouteParams from '../../../types/route-params-type';
 
 function MoviePage(): JSX.Element {
-  const { id } = useParams<RouteParams>();
+  // const { id } = useParams<RouteParams>();
 
   return (
     <>
