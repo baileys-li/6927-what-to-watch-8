5f339801b9c7e78361a125935d4cd58115e0a93d
Arthur Baileys Li <arthurlitovko@gmail.com>
2021-11-06 20:49:05 +0200

    Fix typo

--- project/src/components/small-film-card/index.ts
+++ project/src/components/small-film-card/index.ts
@@ -1,5 +1,5 @@
 import SmallFilmCard from './small-film-card';
-import Sleketon from './small-film-card.skeleton';
+import Skeleton from './small-film-card.skeleton';
 
 export default SmallFilmCard;
-export const SmallFilmCardSkeleton = Sleketon;
+export const SmallFilmCardSkeleton = Skeleton;
